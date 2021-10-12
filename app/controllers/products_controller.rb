e3836db90cfbafb23573770679e110e78dd9d3e1
Esteban Damazio <edamazzio@Estebans-MacBook-Pro.local>
2021-10-12 14:37:16 -0600

    Flash messages on CRUD & Resourceful routing

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,53 @@
 class ProductsController < ApplicationController
+
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
+
   def index
-    
+    @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def update
+    if @product.update(product_params)
+      flash[:notice] = "Product has been updated"
+      redirect_to controller: 'products', action: 'show', id: params[:id]
+    else
+      flash.now[:alert] = "Product has not been updated"
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to root_path
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Product has been saved"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Product has not been saved"
+      render :new
+    end
+  end
+
+  private
+
+  def find_product
+    begin
+      @product = Product.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path
+    end
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-  root to: "products#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "products#index"
+
+  resources :products
+
+  # get "/products", to: "products#index"
+  # get "/products/new", to: "products#new", as: 'new_product'
+  # get "/products/:id", to: "products#show", as: 'product'
+  # get "/products/:id/edit", to: "products#edit", as: 'edit_product'
+  # patch "/products/:id", to: "products#update"
+  # delete "/products/:id", to: "products#destroy"
+
+  post "/products", to: "products#create"
 end
