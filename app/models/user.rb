b914f490d5f307ad0e4ad6ad470a5d1997ad91a7
Esteban Damazio <edamazzio@Estebans-MacBook-Pro.local>
2021-10-27 13:35:27 -0600

    vid 75

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-  validates :first_name, :last_name, :email, presence: true
+  validates :first_name, :last_name, presence: true
   validates :email, presence:true, uniqueness: { case_sensitive: false },
     format: {
       with: /\A[A-Z0-9#-_~!$&'()*+,;=:.]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i
     }
   has_secure_password
+
+  has_many :products, dependent: :destroy
 
   before_save :downcase_email
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  # Sign Up
+  get 'users/new', to: 'users#new', as: 'new_user'
+  get '/signup', to: 'users#new'
+  resources :users, only: [:create]
+
+  # Sign In
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy', as: 'session'
+
+  # Products
   root to: "products#index"
-
   resources :products
 
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get "/products", to: "products#index"
   # get "/products/new", to: "products#new", as: 'new_product'
   # get "/products/:id", to: "products#show", as: 'product'
